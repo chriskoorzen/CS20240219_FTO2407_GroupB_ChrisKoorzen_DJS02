49b52f26cc729f10025faaef06bd5ba2b355f8cc
Chris Koorzen <fckoorzen@gmail.com>
2024-10-21 12:51:36 +0200

    Crash program on text inputs.

--- scripts.js
+++ scripts.js
@@ -23,5 +23,43 @@ form.addEventListener("submit", (event) => {
     return;
   };
 
+  // Crash Program on text inputs
+  try{
+    if (containsText(dividend) || containsText(divider)){
+      throw TypeError("NotANumberError");
+    };
+  } catch (error){
+    console.error(error);
+    document.body.innerHTML = "";
+    document.body.append(
+      Object.assign(document.createElement("h1"), {innerText:"Something critical went wrong. Please reload the page"})
+    );
+    return;
+  };
+
   result.innerText = Math.floor(Number.parseFloat(dividend / divider));   // round down to nearest Integer
 });
+
+
+
+function containsText(numberString){
+  /* 
+    The project brief does not explicitly state whether or not
+    decimal values may be used as inputs (eg. 3 / 1.5).
+    Therefore we have to make allowance for the "." character
+    without leaking loopholes like "....." as an input.
+  */
+
+  const number = Number.parseFloat(numberString);
+  
+  if(Number.isNaN(number)){ // Simple Case
+    return true;
+  };
+
+  /* 
+    Because Number.parse[Int|Float] itself silently ignores mixed inputs,
+    we can do a length comparison after the conversion 
+    to see if we have a valid number.
+  */
+  return !(String(number).length === numberString.length);
+};
